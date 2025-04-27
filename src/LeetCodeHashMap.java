773f71dee73f097f074a52799c3fc54e37db4683
Sarmad <engrsarmadchandio@gmail.com>
2025-04-27 17:48:32 +0500

    groupAnagrams and twoSum

--- src/LeetCodeHashMap.java
+++ src/LeetCodeHashMap.java
@@ -67,6 +67,68 @@ public class LeetCodeHashMap {
 
         return null;
     }
+
+
+//    private static boolean isAnagram(String str1, String str2){
+//        Map<Character, Integer> booleanMap = new HashMap<>();
+//        if (str1.equals(str2)) return true;
+//        if (str1.length() != str2.length()) return false;
+//        for (int i=0;i<str2.length();i++){
+//            char ch = str1.charAt(i);
+//            booleanMap.put(ch, booleanMap.getOrDefault(ch, 0) + 1);
+//        }
+//        for (int j=0;j<str2.length();j++){
+//            char ch = str2.charAt(j);
+//            if (!booleanMap.containsKey(ch)){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    public static List<List<String>> groupAnagrams(String[] strings) {
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+
+        for (String string : strings) {
+            char[] chars = string.toCharArray();
+            Arrays.sort(chars);
+            String canonical = new String(chars);
+
+            if (anagramGroups.containsKey(canonical)) {
+                anagramGroups.get(canonical).add(string);
+            } else {
+                List<String> group = new ArrayList<>();
+                group.add(string);
+                anagramGroups.put(canonical, group);
+            }
+        }
+
+        return new ArrayList<>(anagramGroups.values());
+    }
+
+    //Problem: Given an array of integers nums and a target integer target,
+    // find the indices of two numbers in the array that add up to the target.
+
+    //Input: int[] nums = [2, 7, 11, 15], target = 9
+    //Output: [0, 1]
+    //Explanation: The numbers at indices 0 and 1 in the array add up to the target 9.
+    public static int[] twoSum(int [] array, int target){
+        Map<Integer,Integer> integerIntegerMap = new HashMap<>();
+        for (int i=0 ;i<array.length;i++){
+            //tager = x+y;
+            //x = target-y;
+            int complement = target - array[i];
+            if (integerIntegerMap.containsKey(complement)){
+                return new int [] {integerIntegerMap.get(complement),i};
+            }
+            integerIntegerMap.put(array[i],i);
+        }
+        return new int[0];
+    }
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Commen elments exists? : "+itemsInCommon(new int []{1,2,3},new int []{4,5,6}));
 
@@ -78,6 +140,17 @@ public class LeetCodeHashMap {
 
         System.out.println(SecondNonRepeatingChar("leetcode"));
 
+        System.out.println("******ANGRAMS******");
+        System.out.println("1st set:");
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
 
+        System.out.println("\n2nd set:");
+        System.out.println(groupAnagrams(new String[]{"abc", "cba", "bac", "foo", "bar"}));
+
+        System.out.println("\n3rd set:");
+        System.out.println(groupAnagrams(new String[]{"listen", "silent", "triangle", "integral", "garden", "ranged"}));
+
+        System.out.println("**************");
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 }
